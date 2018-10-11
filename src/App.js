e48926965b2f98ea97c75ad6d8d8aeebc04b4fd0
danielgssp <danielgssp@hotmail.com>
2018-10-11 15:22:49 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -66,9 +66,12 @@ class App extends Component {
     /*analytics.send('post', body);
     analytics.GA('create',body);*/
 
-    analytics.GA('send', {
+    /*analytics.GA('send', {
       'hitType': 'event', 'eventCategory': 'clicked on create', 'eventAction': 'clickAction', 'eventLabel': 'First Event' }
-    );
+    );*/
+
+    analytics.GA('send', 'event', 'category', 'action', body);
+    
   }
   
   render () {
